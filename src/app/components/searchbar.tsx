c2786db99b1389522772b2dd522b269046e2e0c1
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-07-14 18:34:01 +0100

    styling added to searchbar component

--- src/app/components/searchbar.tsx
+++ src/app/components/searchbar.tsx
@@ -1,8 +1,8 @@
 export default function Searchbar() {
     return (
-        <div className="searchbar">
-            <input type="text" placeholder="Search..." />
-            <button type="submit">Search</button>
+        <div className="flex justify-center items-center border-blue-500 border-2 text-white p-4 mt-4">
+            <input className="border-white border-[1px] p-4" type="text" placeholder="Search..." />
+            <button className="border-white border-[1px] p-4" type="submit">Search</button>
         </div>
     );
 }
